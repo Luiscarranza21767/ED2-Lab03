eee5b96429814c6336372db708f1b8e1004d8494
Luiscarranza21767 <car21767@uvg.edu.gt>
2023-02-02 21:23:33 -0600

    Se creó el archivo del proyecto

---
+++ ED2-Lab03.X/main.c
@@ -0,0 +1,18 @@
+/* 
+ * File:   main.c
+ * Author: Pablo
+ *
+ * Created on 2 de febrero de 2023, 06:44 PM
+ */
+
+#include <stdio.h>
+#include <stdlib.h>
+
+/*
+ * 
+ */
+int main(int argc, char** argv) {
+
+    return (EXIT_SUCCESS);
+}
+
